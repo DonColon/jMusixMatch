024d23659cde10d6d2b6c97c532987b60fd4cd90
DonColon <drrafshi@hotmail.de>
2019-07-19 00:45:45 +0200

    Defined API endpoints, status codes and query parameters

---
+++ src/main/java/com/dardan/rrafshi/jmusixmatch/Constants.java
@@ -0,0 +1,42 @@
+package com.dardan.rrafshi.jmusixmatch;
+
+public final class Constants
+{
+	public static final String API_URL = "https://api.musixmatch.com/ws/";
+	public static final String API_VERSION = "1.1";
+	public static final String API_KEY = "apikey";
+
+	public static final String TRACK_ID = "track_id";
+	public static final String ARTIST_ID = "artist_id";
+	public static final String ALBUM_ID = "album_id";
+	public static final String COMMONTRACK_ID = "commontrack_id";
+	public static final String TRACK_MBID = "track_mbid";
+	public static final String ARTIST_MBID = "artist_mbid";
+	public static final String ALBUM_MBID = "album_mbid";
+
+	public static final String QUERY_TRACK = "q_track";
+	public static final String QUERY_ARTIST = "q_artist";
+	public static final String QUERY_LYRICS = "q_lyrics";
+	public static final String QUERY = "q";
+
+	public static final String FILTER_HAS_LYRICS = "f_has_lyrics";
+	public static final String FILTER_IS_INSTRUMENTAL = "f_is_instrumental";
+	public static final String FILTER_HAS_SUBTITLE = "f_has_subtitle";
+	public static final String FILTER_MUSIC_GENRE_ID = "f_music_genre_id";
+	public static final String FILTER_SUBTITLE_LENGTH = "f_subtitle_length";
+	public static final String FILTER_SUBTITLE_LENGTH_MAX_DEVIATION = "f_subtitle_length_max_deviation";
+	public static final String FILTER_LYRICS_LANGUAGE = "f_lyrics_language";
+	public static final String FILTER_ARTIST_ID = "f_artist_id";
+	public static final String FILTER_ARTIST_MBID = "f_artist_mbid";
+
+	public static final String SORT_TRACK_RATING = "s_track_rating";
+	public static final String SORT_TRACK_RELEASE_DATE = "s_track_release_date";
+	public static final String SORT_ARTIST_RATING = "s_artist_rating";
+
+	public static final String PAGE = "page";
+	public static final String PAGE_SIZE = "page_size";
+	public static final String FORMAT = "format";
+
+
+	private Constants() {}
+}
---
+++ src/main/java/com/dardan/rrafshi/jmusixmatch/Endpoints.java
@@ -0,0 +1,28 @@
+package com.dardan.rrafshi.jmusixmatch;
+
+public final class Endpoints
+{
+	public static final String ALBUM_GET = "album.get";
+	public static final String ALBUM_TRACKS_GET = "album.tracks.get";
+
+	public static final String ARTIST_GET = "artist.get";
+	public static final String ARTIST_RELATED_GET = "artist.related.get";
+	public static final String ARTIST_ALBUMS_GET = "artist.albums.get";
+	public static final String ARTIST_SEARCH = "artist.search";
+
+	public static final String TRACK_GET = "track.get";
+	public static final String TRACK_LYRICS_GET = "track.lyrics.get";
+	public static final String TRACK_SUBTITLE_GET = "track.subtitle.get";
+	public static final String TRACK_SNIPPET_GET = "track.snippet.get";
+	public static final String TRACK_SEARCH = "track.search";
+
+	public static final String MATCHER_TRACK_GET = "matcher.track.get";
+	public static final String MATCHER_LYRICS_GET = "matcher.lyrics.get";
+	public static final String MATCHER_SUBTITLE_GET = "matcher.subtitle.get";
+
+	public static final String CHART_TRACKS_GET = "chart.tracks.get";
+	public static final String CHART_ARTISTS_GET = "chart.artists.get";
+
+
+	public Endpoints() {}
+}
